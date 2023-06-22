9c5f39114dd2a833eaf8eb9c46d9635547cdd1c9
hicass <cassidyjwalters@gmail.com>
2023-06-22 16:31:42 -0700

    Inital commit, add MERN-Stack Infrastructure, and token-based auth.

--- src/pages/App/App.jsx
+++ src/pages/App/App.jsx
@@ -1,25 +1,28 @@
 import { useState } from 'react';
-import { Routes, Route } from 'react-router-dom';
+import { Link } from 'react-router-dom';
 import { getUser } from '../../utilities/users-service';
-import './App.css';
+import * as userService from '../../utilities/users-service';
 import AuthPage from '../AuthPage/AuthPage';
-import NewOrderPage from '../NewOrderPage/NewOrderPage';
-import OrderHistoryPage from '../OrderHistoryPage/OrderHistoryPage';
-import NavBar from '../../components/NavBar/NavBar';
+import './App.css';
 
 export default function App() {
   const [user, setUser] = useState(getUser());
+
+  function handleLogOut() {
+    userService.logOut();
+    setUser(null);
+  }
 
   return (
     <main className="App">
       { user ?
           <>
-            <NavBar user={user} setUser={setUser} />
-            <Routes>
-              {/* Route components in here */}
-              <Route path="/orders/new" element={<NewOrderPage />} />
-              <Route path="/orders" element={<OrderHistoryPage />} />
-            </Routes>
+            <h1>You are logged in!</h1>
+            <nav>
+            <span>Welcome, {user.name}</span>
+            &nbsp;&nbsp;
+            <Link to="" onClick={handleLogOut}>Log Out</Link>
+            </nav>
           </>
           :
           <AuthPage setUser={setUser} />
